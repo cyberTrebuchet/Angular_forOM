e3fec55f7e3ee916d96a8ea57889679d9824dcb8
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-09-25 17:59:06 -0400

    hack, hack, hackin on heaven's door

--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -5,4 +5,13 @@ angular.module('forOM', [])
 'posts',
 function($scope, $stateParams, posts){
   $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
 }]);
