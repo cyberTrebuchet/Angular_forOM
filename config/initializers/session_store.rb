4bcab9623daab20129fdb7eb0dbd4a02b34ca93b
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-01 12:34:29 -0400

    from the top again, people

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_Angular_on_Rails_forOM_session'
--- vendor/assets/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-//= require angular
-//  just because https://thinkster.io/angular-rails#integrating-the-front-end-with-the-asset-pipeline told me to:
-//= require angular-rails-templates
-//= require angular-ui-router
-//= require_tree .
